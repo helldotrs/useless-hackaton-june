e606d2a0fcd0b983830ba1114435fb4f3e4046aa
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-06-16 18:47:16 +0200

    Update useless.py

--- useless.py
+++ useless.py
@@ -5,7 +5,8 @@ import random
 list_one = ["bottle","pizza","phone","app", "plant", "hack", "kim kardashian"]
 list_two = ["screams","cries","scratch","stops","guesses", "hacks"]
 
-word_one = random.randint(0,len(list_one))
-word_two = random.randint(0,len(list_two))
+word_one = list_one[random.randint(0,len(list_one))]
+word_two = list_two[random.randint(0,len(list_two))]
 
-print ("the worst idea possible a {word_one} that {word_two}")
+print (f"the worst idea possible a {word_one} that {word_two}")
+print (f"{word_one} that {word_two}")
